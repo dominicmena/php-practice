2022bae436bbc7d05a67ba813320d9b3d9664856
dominicmena <mena.dominic@gmail.com>
2024-04-01 17:34:41 -0500

    updated with composer method slugify

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 
-$dogName = 'barksAlot';
+require __DIR__ . '/vender/autoload.php'; 
+// autoloads the files ^^
+use Cocur\Slugify\Slugify;
+// same as import in node
+
+$slugify = new Slugify();
+echo $slugify->slugify('the sky is blue and the grass is green');
+
+// $dogName = 'barksAlot';
 
 // echo 'the dogs name is ' . $dogName . ' for sure';
 
@@ -8,21 +16,23 @@ $dogName = 'barksAlot';
 // echo "the name of the dog is $dogName";
 // double quotes allows for string interpolation
 
-function doubleMe($x) {
-    return $x * 2;
-}
+// function doubleMe($x) {
+//     return $x * 2;
+// }
 
-function tripleMe($x) {
-    return $x * 3;
-}
+// function tripleMe($x) {
+//     return $x * 3;
+// }
 
 
 // echo tripleMe(doubleMe(10));
 
-$ourNumber = tripleMe(doubleMe(100));
+// $ourNumber = tripleMe(doubleMe(100));
 
-if ($ourNumber > 200) {
-    echo 'the number is larger'; 
-} else {
-    echo 'the number is smaller';
-}
+// if ($ourNumber > 200) {
+//     echo 'the number is larger'; 
+// } else {
+//     echo 'the number is smaller';
+// }
+
+// using server by typing php -S localhost:8000, also renamed file to index.php
