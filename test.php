1faa6c16de7bde83248cd550998d1e5277b5471f
dominicmena <mena.dominic@gmail.com>
2024-04-01 17:14:07 -0500

    updated functions, and if/else

--- test.php
+++ test.php
@@ -2,4 +2,27 @@
 
 $dogName = 'barksAlot';
 
-echo 'the dogs name is ' . $dogName . ' for sure';
+// echo 'the dogs name is ' . $dogName . ' for sure';
+
+// use a period for concatenation ^^^
+// echo "the name of the dog is $dogName";
+// double quotes allows for string interpolation
+
+function doubleMe($x) {
+    return $x * 2;
+}
+
+function tripleMe($x) {
+    return $x * 3;
+}
+
+
+// echo tripleMe(doubleMe(10));
+
+$ourNumber = tripleMe(doubleMe(100));
+
+if ($ourNumber > 200) {
+    echo 'the number is larger'; 
+} else {
+    echo 'the number is smaller';
+}
